edbc55d1f57161f7fd705ab727e3d212702a0819
quantitychar <olenabogoslavska7@gmail.com>
2025-06-01 22:20:31 +0100

    refactor: wrap App with BrowserRouter and nest providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,25 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
+import App from "./App.jsx";
+
+import { ModalProvider } from "./hooks/useModal.jsx";
 import { CartProvider } from "./contexts/CartContext.jsx";
 import { FavoritesProvider } from "./contexts/FavoritesContext.jsx";
-import { ModalProvider } from "./hooks/useModal.jsx";
-
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ModalProvider>
-      <CartProvider>
-        <FavoritesProvider>
-          <App />
-        </FavoritesProvider>
-      </CartProvider>
-    </ModalProvider>
+    <BrowserRouter>
+      <ModalProvider>
+        <CartProvider>
+          <FavoritesProvider>
+            <App />
+          </FavoritesProvider>
+        </CartProvider>
+      </ModalProvider>
+    </BrowserRouter>
   </StrictMode>
 );
